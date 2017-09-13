7ba7f8246da2b2d96d94a8b1db5a9a45c6dd6aa8
jamespeterson <jamespeterson0918@yahoo.com>
2017-09-13 17:00:31 -0400

    done with 3 of 4 rows

---
+++ apps.js
@@ -0,0 +1,159 @@
+$(() => {
+
+    $('.mT').on('click', function(){
+        var y = window.prompt("What is the highest grossing film of all time?")
+        if(y === "Avatar") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+        
+    $('.90s').on('click', function(){
+    var y = window.prompt("Which rock group released the album NEVERMIND in 1991?")
+    if(y === "Nirvana") { 
+    window.alert('correct')
+            
+    } else { 
+     window.alert('incorrect')
+                }
+    })
+
+    $('.rnr').on('click', function(){
+        var y = window.prompt("Which Linkin Park album is certified Diamond?")
+        if(y === "Hybrid Theory") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.gameS').on('click', function(){
+        var y = window.prompt("What game had the most online activity during the 2000s")
+        if(y === "Halo 2") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.horrorMovie').on('click', function(){
+        var y = window.prompt("Who made their 1972 film debut with The Last House on the Left?")
+        if(y === "Wes Craven") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.mTT').on('click', function(){
+        var y = window.prompt("Who wrote and directed the 1976 film Rocky?")
+        if(y === "Sylvester Stallone") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.90ss').on('click', function(){
+        var y = window.prompt("Which band released 1994s The Downward Spiral album")
+        if(y === "Nine Inch Nails") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.rnr2').on('click', function(){
+        var y = window.prompt("What is the name of Green Day's seventh studio album?")
+        if(y === "Green Day") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.gameSS').on('click', function(){
+        var y = window.prompt("What was the highest selling Playstation2 game of the 2000s?")
+        if(y === "GTA San Andreas") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.horrorMOvie').on('click', function(){
+        var y = window.prompt("What is the name of Rob Zombie's first feature film debut?")
+        if(y === "House of 1000 Corpses") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.mTTT').on('click', function(){
+        var y = window.prompt("What is the highest grossing Marvel film to date?")
+        if(y === "Captain America Civil War") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.90sss').on('click', function(){
+        var y = window.prompt("Who was the lead singer of the rock band Nirvana?")
+        if(y === "Kurt Cobain") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.rnr3').on('click', function(){
+        var y = window.prompt("What is the name of the metal song featured at the end of the film, Vampires Suck?")
+        if(y === "If I Was Your Vampire") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+    $('.gameSSS').on('click', function(){
+        var y = window.prompt("What Zelda title was first released on Nintendo Gamecube?")
+        if(y === "Windwaker") { 
+            window.alert('correct')
+    
+        } else { 
+            window.alert('incorrect')
+        }
+    })
+
+
+
+
+
+
+
+add400 = () => {
+    var y = window.prompt("what is my name")
+    if(y === "James") { 
+        window.alert('correct')
+
+    } else { 
+        window.alert('incorrect')
+    }
+
+}
+})
