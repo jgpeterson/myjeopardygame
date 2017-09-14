4dcb2299f66e2d959114363ca2ca750d99a3a04d
jamespeterson <jamespeterson0918@yahoo.com>
2017-09-14 14:36:11 -0400

    reaching the end

--- apps.js
+++ apps.js
@@ -1,12 +1,16 @@
 $(() => {
+    let score = 0;
 
     $('.mT').on('click', function(){
         var y = window.prompt("What is the highest grossing film of all time?")
         if(y === "Avatar") { 
             window.alert('correct')
-    
+            score += 100;
+            $("#score").html(score);
         } else { 
             window.alert('incorrect')
+            score -= 100;
+            $("#score").html(score);
         }
     })
         
@@ -14,9 +18,12 @@ $(() => {
     var y = window.prompt("Which rock group released the album NEVERMIND in 1991?")
     if(y === "Nirvana") { 
     window.alert('correct')
-            
+           score+= 100;
+           $("#score").html(score) 
     } else { 
      window.alert('incorrect')
+     score -= 100;
+     $("#score").html(score);
                 }
     })
 
@@ -24,9 +31,12 @@ $(() => {
         var y = window.prompt("Which Linkin Park album is certified Diamond?")
         if(y === "Hybrid Theory") { 
             window.alert('correct')
-    
+            score += 100;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 100;
+            $("#score").html(score)
         }
     })
 
@@ -34,9 +44,12 @@ $(() => {
         var y = window.prompt("What game had the most online activity during the 2000s")
         if(y === "Halo 2") { 
             window.alert('correct')
-    
+            score += 100;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 100;
+            $("#score").html(score)
         }
     })
 
@@ -44,9 +57,12 @@ $(() => {
         var y = window.prompt("Who made their 1972 film debut with The Last House on the Left?")
         if(y === "Wes Craven") { 
             window.alert('correct')
-    
+            score += 100;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 100;
+            $("#score").html(score)
         }
     })
 
@@ -54,9 +70,13 @@ $(() => {
         var y = window.prompt("Who wrote and directed the 1976 film Rocky?")
         if(y === "Sylvester Stallone") { 
             window.alert('correct')
+            score += 200;
+            $("#score").html(score)
     
         } else { 
             window.alert('incorrect')
+            score -= 200;
+            $("#score").html(score)
         }
     })
 
@@ -64,9 +84,12 @@ $(() => {
         var y = window.prompt("Which band released 1994s The Downward Spiral album")
         if(y === "Nine Inch Nails") { 
             window.alert('correct')
-    
+            score += 200;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 200;
+            $("#score").html(score)
         }
     })
 
@@ -74,9 +97,12 @@ $(() => {
         var y = window.prompt("What is the name of Green Day's seventh studio album?")
         if(y === "Green Day") { 
             window.alert('correct')
-    
+            score += 200;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 200;
+            $("#score").html(score)
         }
     })
 
@@ -84,9 +110,12 @@ $(() => {
         var y = window.prompt("What was the highest selling Playstation2 game of the 2000s?")
         if(y === "GTA San Andreas") { 
             window.alert('correct')
-    
+            score += 200;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 200;
+            $("#score").html(score)
         }
     })
 
@@ -94,9 +123,12 @@ $(() => {
         var y = window.prompt("What is the name of Rob Zombie's first feature film debut?")
         if(y === "House of 1000 Corpses") { 
             window.alert('correct')
-    
+            score += 200;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 200;
+            $("#score").html(score)
         }
     })
 
@@ -104,9 +136,12 @@ $(() => {
         var y = window.prompt("What is the highest grossing Marvel film to date?")
         if(y === "Captain America Civil War") { 
             window.alert('correct')
-    
+            score += 300;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 300;
+            $("#score").html(score)
         }
     })
 
@@ -114,9 +149,12 @@ $(() => {
         var y = window.prompt("Who was the lead singer of the rock band Nirvana?")
         if(y === "Kurt Cobain") { 
             window.alert('correct')
-    
+            score += 300;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 300;
+            $("#score").html(score)
         }
     })
 
@@ -124,9 +162,12 @@ $(() => {
         var y = window.prompt("What is the name of the metal song featured at the end of the film, Vampires Suck?")
         if(y === "If I Was Your Vampire") { 
             window.alert('correct')
-    
+            score += 300;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 300;
+            $("#score").html(score)
         }
     })
 
@@ -134,9 +175,12 @@ $(() => {
         var y = window.prompt("What Zelda title was first released on Nintendo Gamecube?")
         if(y === "Windwaker") { 
             window.alert('correct')
-    
+            score += 300;
+            $("#score").html(score)
         } else { 
             window.alert('incorrect')
+            score -= 300;
+            $("#score").html(score)
         }
     })
 
@@ -144,8 +188,95 @@ $(() => {
         var y = window.prompt("What American remake of a Japanese horror film was released in the United States in 2002?")
         if(y === "The Ring") { 
             window.alert('correct')
+            score += 300;
+            $("#score").html(score)
+        } else { 
+            window.alert('incorrect')
+            score -= 300;
+            $("#score").html(score)
+        }
+    })
+
+    $('.mTTTT').on('click', function(){
+        var y = window.prompt("What is the name of Julia Roberts character in the film Pretty Woman?")
+        if(y === "Vivian Ward") { 
+            window.alert('correct')
+            score += 400;
+            $("#score").html(score)
+        } else { 
+            window.alert('incorrect')
+            score -= 400;
+            $("#score").html(score)
+        }
+    })
+
+    $('.90ssss').on('click', function(){
+        var y = window.prompt("What is the name of Marilyn Mansons second studio album?")
+        if(y === "Antichrist Superstar") { 
+            window.alert('correct')
+            score += 400;
+            $("#score").html(score)
+        } else { 
+            window.alert('incorrect')
+            score -= 400;
+            $("#score").html(score)
+        }
+    })
+    
+    $('.rnr4').on('click', function(){
+        var y = window.prompt("Which rapper had an album titled Lifestylez ov da poor an danderouz?")
+        if(y === "Big L") { 
+            window.alert('correct')
+            score += 400;
+            $("#score").html(score)
+        } else { 
+            window.alert('incorrect')
+            score -= 400;
+            $("#score").html(score)
+        }
+    })
+
+    $('.gameSSSS').on('click', function(){
+        var y = window.prompt("What year was the Xbox title Halo 2 released?")
+        if(y === "2004") { 
+            window.alert('correct')
+            score += 400;
+            $("#score").html(score)
+        } else { 
+            window.alert('incorrect')
+            score -= 400;
+            $("#score").html(score)
+        }
+    })
+    
+    $('.horrorMOVIE').on('click', function(){
+        var y = window.prompt("What horror movie started the slasher craze?")
+        if(y === "Peeping Tom") { 
+            window.alert('correct')
+            score += 400;
+            $("#score").html(score)
     
         } else { 
             window.alert('incorrect')
+            score -= 400;
+            $("#score").html(score)
         }
     })
+
+
+
+
+
+
+
+add400 = () => {
+    var y = window.prompt("what is my name")
+    if(y === "James") { 
+        window.alert('correct')
+
+    } else { 
+        window.alert('incorrect')
+    }
+
+}
+})
